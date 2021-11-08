c26438f97333c9ea1bf1eeb15f033489e595e24e
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-08 15:42:40 -0300

    adds the value contato in the end of the array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4..js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4..js
@@ -1,4 +1,3 @@
-//procure o índice do valor "Portfólio" do array menu
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let indexOfPortfolio = menu.indexOf("Portfólio");
-console.log(indexOfPortfolio);
+menu.push("Contato")
+console.log(menu);
