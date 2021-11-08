a00b42b531977f1f7c838e43bcd187bc18d50ddb
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-08 16:01:52 -0300

    feat: adds for/of to iterate through an array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4..js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4..js
@@ -1,5 +1,5 @@
-//Utilize o for para imprimir os elementos da lista groceryList com o console.log() 
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
-for (index = 0; index < groceryList.length; index += 1) {
-    console.log(groceryList[index]);
+//Utilize o for/of para imprimir os elementos da lista names com o console.log()
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (nome of names) {
+    console.log(nome)
 }
