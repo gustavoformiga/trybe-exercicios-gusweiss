d4383e8e7e98b4228885d431abd14efd99cb0018
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-08 15:57:30 -0300

    feat: adds a for loop at the groceryList

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4..js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4..js
@@ -1,3 +1,5 @@
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push("Contato")
-console.log(menu);
+//Utilize o for para imprimir os elementos da lista groceryList com o console.log() 
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]);
+}
