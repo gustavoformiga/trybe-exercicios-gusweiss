d3d87fc44e4106f31d3810209ad8b05c48fe7c8b
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-08 14:01:19 -0300

    feat: adds subtraction, multiplication, division and module programs

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/bloco-4-introducao-a-javascript-e-logica-de-programacao-dia-1-javascript-primeiros-passos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/bloco-4-introducao-a-javascript-e-logica-de-programacao-dia-1-javascript-primeiros-passos.js
@@ -1,5 +1,21 @@
 //parte1-adição
-let a = 5;
+let a = 50;
 let b = 10;
 let soma  = (a + b);
 console.log(soma);
+
+//parte1-subtração
+let subtracao  = (a - b);
+console.log(subtracao);
+
+//parte1-multiplicação
+let multiplicacao = (a * b)
+console.log(multiplicacao);
+
+//parte1-divisão
+let divisao = (a / b)
+console.log(divisao);
+
+//parte1-módulo
+let modulo = (a % b);
+console.log(modulo);
