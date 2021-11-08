2b356ea126c202bb8f6eb75afefd81882341399e
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-08 18:29:19 -0300

    part4-printing a different  message for average <20 and average >20

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
@@ -1,8 +1,14 @@
-//parte3-Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+//parte4-Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 for (let i = 0; i < numbers.length; i += 1) {
     soma = soma + numbers[i];
     var media = soma / numbers.length
 }
-console.log(media);
+if (media > 20) {
+    console.log("valor maior que 20");
+} else {
+    console.log("valor menor ou igual a 20");
+}
+
+
