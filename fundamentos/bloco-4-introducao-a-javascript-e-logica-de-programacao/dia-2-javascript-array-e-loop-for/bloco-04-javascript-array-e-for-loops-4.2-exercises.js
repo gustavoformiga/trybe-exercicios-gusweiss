43dd921efa7c7f61cf8cd1650a2051e6c5a0a0ed
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-08 18:20:16 -0300

    part2-sum of all array elements

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
@@ -1,5 +1,7 @@
-//parte1-Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
+//parte2-Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (number of numbers) {
-    console.log(number);
+let soma = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+    soma = soma + numbers[i];
 }
+console.log(soma);
