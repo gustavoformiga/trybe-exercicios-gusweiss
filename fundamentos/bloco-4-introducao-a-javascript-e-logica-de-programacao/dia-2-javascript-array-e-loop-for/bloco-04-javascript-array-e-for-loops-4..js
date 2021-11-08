5377353585b0dbcbf12bdf4c4b0d25459a681152
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-08 15:41:09 -0300

    feat: adds search for index of Portfólio

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4..js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4..js
@@ -1,4 +1,4 @@
-//Obtenha o valor "Serviços"do array menu
+//procure o índice do valor "Portfólio" do array menu
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu[1];
-console.log(menuServices);
+let indexOfPortfolio = menu.indexOf("Portfólio");
+console.log(indexOfPortfolio);
