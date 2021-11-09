57825e678a7baa30fea9c6f29276caf0f90bc45d
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-09 10:05:29 -0300

    part6-finding how many odd values there are in the array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
@@ -1,9 +1,12 @@
-//parte5-Utilizando for , descubra qual o maior valor contido no array e imprima-o;
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let largestNumber = 0;
-for (i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] > largestNumber) {
-        largestNumber = numbers[i];
+//parte6-Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+let numbers = [5, 6, 9, 1, 20, 40, 2];
+let oddNumbers = 0;
+for (let i = 0; i < numbers.length; i += 1) {
+    let verifyingOdd = numbers[i] % 2;
+    if (verifyingOdd != 0) {
+        oddNumbers = oddNumbers + 1;
+    } else if (oddNumbers === 0) {
+        console.log("nenhum valor ímpar encontrado")
     }
-}
-console.log(largestNumber);
+} 
+console.log(oddNumbers);
