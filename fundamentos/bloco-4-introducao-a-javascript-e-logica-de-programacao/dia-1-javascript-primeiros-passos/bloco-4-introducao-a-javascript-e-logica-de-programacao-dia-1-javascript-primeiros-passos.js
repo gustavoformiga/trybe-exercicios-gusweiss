f48f7b996854dc1621c6fc8fd1a261ae084e1d04
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-09 16:02:02 -0300

    part11-program that calculates salary discounting Inss an IR

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/bloco-4-introducao-a-javascript-e-logica-de-programacao-dia-1-javascript-primeiros-passos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/bloco-4-introducao-a-javascript-e-logica-de-programacao-dia-1-javascript-primeiros-passos.js
@@ -138,3 +138,27 @@ if (productCost >= 0 && productPrice >= 0) {
     console.log("Warning: unacceptable values");
 }
 
+//part11- Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+var salarioBruto = ;
+var salrioLiquido = 0;
+if (salarioBruto <= 1556.94) {
+    salarioBase = salarioBruto - (salarioBruto * 0.08);
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+    salarioBase = salarioBruto - (salarioBruto * 0.09);
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+    salarioBase = salarioBruto - (salarioBruto * 0.11);
+} else {
+    salarioBase = salarioBruto - 570.88;
+} 
+if (salarioBase <= 1903.98) {
+    salarioLiquido = salarioBase;
+} else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+    salarioLiquido = salarioBase - ((salarioBase * 0.075)- 142.80);
+} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+    salarioLiquido = salarioBase - ((salarioBase * 0.15)- 354.80);
+} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+    salarioLiquido = salarioBase - ((salarioBase * 0.225)- 636.13);
+} else {
+    salarioLiquido = salarioBase - ((salarioBase * 0.275)- 869.36);
+}
+console.log(salarioLiquido);
