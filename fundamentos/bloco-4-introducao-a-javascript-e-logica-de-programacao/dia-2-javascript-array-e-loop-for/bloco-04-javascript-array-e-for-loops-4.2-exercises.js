1b20e2b68d55818d64f2903c42fda674051a762c
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-09 10:10:05 -0300

    part8- build an array from 1 to 25 and print the result

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
@@ -1,9 +1,6 @@
-//parte7-Utilizando for , descubra qual o menor valor contido no array e imprima-o;
-let numbers = [5, 6, 9, 1, 20, 40, 2];
-let menorNumero = Infinity;
-for (i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] < menorNumero) {
-        menorNumero = numbers[i]
-    }
+//parte8-Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+let = newArray = [];
+for (i = 1; i <= 25; i += 1) {
+    newArray.push(i);
 }
-console.log(menorNumero);
+console.log(newArray); 
