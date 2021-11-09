0ee7f5064471c04f4bf558cc4027b4267f9af3e5
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-09 10:07:48 -0300

    part7- finding the minimum value in the array numbers

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
@@ -1,12 +1,9 @@
-//parte6-Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+//parte7-Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 let numbers = [5, 6, 9, 1, 20, 40, 2];
-let oddNumbers = 0;
-for (let i = 0; i < numbers.length; i += 1) {
-    let verifyingOdd = numbers[i] % 2;
-    if (verifyingOdd != 0) {
-        oddNumbers = oddNumbers + 1;
-    } else if (oddNumbers === 0) {
-        console.log("nenhum valor ímpar encontrado")
+let menorNumero = Infinity;
+for (i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] < menorNumero) {
+        menorNumero = numbers[i]
     }
-} 
-console.log(oddNumbers);
+}
+console.log(menorNumero);
