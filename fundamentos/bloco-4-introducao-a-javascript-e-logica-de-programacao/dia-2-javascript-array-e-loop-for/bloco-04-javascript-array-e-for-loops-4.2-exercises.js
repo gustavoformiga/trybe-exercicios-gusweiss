059a537572a007e7101c7e1bde506a51ac969032
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-09 09:55:30 -0300

    part5-finding the largest number in a array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
@@ -1,14 +1,9 @@
-//parte4-Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+//parte5-Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
-for (let i = 0; i < numbers.length; i += 1) {
-    soma = soma + numbers[i];
-    var media = soma / numbers.length
+let largestNumber = 0;
+for (i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > largestNumber) {
+        largestNumber = numbers[i];
+    }
 }
-if (media > 20) {
-    console.log("valor maior que 20");
-} else {
-    console.log("valor menor ou igual a 20");
-}
-
-
+console.log(largestNumber);
