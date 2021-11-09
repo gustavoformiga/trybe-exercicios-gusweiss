a6a367b0594c125a389e19f7f99400199a2ee814
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-09 10:13:02 -0300

    part9- print the division by 2 of the array elements

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/bloco-04-javascript-array-e-for-loops-4.2-exercises.js
@@ -1,6 +1,9 @@
-//parte8-Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+//parte9-Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
 let = newArray = [];
 for (i = 1; i <= 25; i += 1) {
     newArray.push(i);
 }
-console.log(newArray); 
+for (i = 0; i < newArray.length; i +=1) {
+    let testDivision = newArray[i] / 2;
+    console.log(testDivision);
+}
