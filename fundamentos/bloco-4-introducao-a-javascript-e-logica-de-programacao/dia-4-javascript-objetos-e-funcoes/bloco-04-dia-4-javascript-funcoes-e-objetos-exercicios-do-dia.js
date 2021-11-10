4ac0f2b5be080803564b21899df680bc9a162279
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-10 20:18:32 -0300

    part 6- accessing an object inside an array that is inside an object

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bloco-04-dia-4-javascript-funcoes-e-objetos-exercicios-do-dia.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bloco-04-dia-4-javascript-funcoes-e-objetos-exercicios-do-dia.js
@@ -1,20 +1,16 @@
 
-// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  recorrente: 'Sim',
+// 6- Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "'O Pior Dia de Todos'",
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
 };
-let info2 = {
-  personagem: 'Tio patinhas',
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: 'O último Macpatinhas',
-  recorrente: 'Sim',
-};
-
-for (let key in info, info2) {
-  console.log(info[key] + " " + "e" + " " + info2[key]);
-}
-
+console.log("O livro favorito de" + " " + leitor.nome, leitor.sobrenome, "se chama" + " " + leitor.livrosFavoritos[0].titulo);
   
