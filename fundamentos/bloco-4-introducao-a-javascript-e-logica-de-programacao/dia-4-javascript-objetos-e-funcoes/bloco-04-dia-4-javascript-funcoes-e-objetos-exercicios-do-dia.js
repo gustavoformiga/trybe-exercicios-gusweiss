8365f9e62c9ad361d7dc9d21c10cfdcbb1d1691c
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-10 19:30:50 -0300

    part2- adding a property to an object

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bloco-04-dia-4-javascript-funcoes-e-objetos-exercicios-do-dia.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bloco-04-dia-4-javascript-funcoes-e-objetos-exercicios-do-dia.js
@@ -1,7 +1,11 @@
-// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+
+// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
-console.log("Bem-vinda," + " " + info.personagem);
+info.recorrente = "Sim"
+console.log(info);
+
+  
