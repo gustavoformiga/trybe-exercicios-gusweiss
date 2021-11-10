7718a51c3ef1180b1d7a0683eacd4bd9a0680147
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-10 20:02:41 -0300

    part 5- adding a second object and iterating through two objects with one for loop

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bloco-04-dia-4-javascript-funcoes-e-objetos-exercicios-do-dia.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bloco-04-dia-4-javascript-funcoes-e-objetos-exercicios-do-dia.js
@@ -1,13 +1,20 @@
 
-// 4- Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   recorrente: 'Sim',
 };
-for (let key in info) {
-  console.log(info[key]);
+let info2 = {
+  personagem: 'Tio patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último Macpatinhas',
+  recorrente: 'Sim',
+};
+
+for (let key in info, info2) {
+  console.log(info[key] + " " + "e" + " " + info2[key]);
 }
 
   
