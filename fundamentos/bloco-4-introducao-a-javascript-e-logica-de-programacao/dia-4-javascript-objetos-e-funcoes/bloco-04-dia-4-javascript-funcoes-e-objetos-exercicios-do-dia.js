25dd56771b4f1351d85b9a72a244430d2cd2a4a9
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-10 19:42:21 -0300

    part 4- do a for/in and show the values of an object

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bloco-04-dia-4-javascript-funcoes-e-objetos-exercicios-do-dia.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bloco-04-dia-4-javascript-funcoes-e-objetos-exercicios-do-dia.js
@@ -1,11 +1,13 @@
 
-// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+// 4- Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
 };
-info.recorrente = "Sim"
-console.log(info);
+for (let key in info) {
+  console.log(info[key]);
+}
 
   
