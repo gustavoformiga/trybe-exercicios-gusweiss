97115365bbb1a9046aefdde25aedf5abcc5968f9
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-10 16:14:51 -0300

    part6- Iteraing through an object with for in and printing the keys and values

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bloco-04-dia-4-javascript-objetos-e-funcoes-fixacao.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bloco-04-dia-4-javascript-objetos-e-funcoes-fixacao.js
@@ -9,11 +9,21 @@ console.log("A jogadora" + " " + "possui:" + " " + player.medals.golden + " " + 
  */
 
 // part for of and for in
-let names = {
+/* let names = {
 person1: 'João',
 person2: 'Maria',
 person3: 'Jorge',
 }; 
 for (let index in names) {
   console.log("Olá" + " " + names[index]);
+} */
+
+// part2 for of and for in
+let car = {
+model: 'A3 Sedan',
+manufacturer: 'Audi',
+year: 2020
+};
+for (let index in car) {
+  console.log(index + ":" + " " + car[index])
 }
