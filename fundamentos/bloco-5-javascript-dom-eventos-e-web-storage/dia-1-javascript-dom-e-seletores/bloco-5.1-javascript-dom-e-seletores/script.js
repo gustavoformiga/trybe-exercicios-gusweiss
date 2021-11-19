d98e693d44415350928b6e3a32a13fbb9ec7472c
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-19 12:21:18 -0300

    feat: fixation exercise- parts 4,5,6,7

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/bloco-5.1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,18 @@
+document.getElementById("header-container").style.color = "black";
+
+document.querySelectorAll("h3")[0].style.color = "black";
+
+document.querySelectorAll("h3")[1].style.color = "black";
+
+document.querySelectorAll("h3")[2].style.color = "gray";
+
+document.querySelectorAll("h3")[3].style.color = "gray";
+
+
+
+
+
+
+document.getElementById("footer-container").style.color = "black";
+
+
--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/dia-2-javascript-trabalhando-com-elementos-fixacao/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/dia-2-javascript-trabalhando-com-elementos-fixacao/script.js
@@ -1,3 +1,21 @@
+// bloco-5-5.2-trabalhando-com-elementos-fixacao
+// 1-Acesse o elemento elementoOndeVoceEsta .
 console.log(document.getElementById("elementoOndeVoceEsta"));
+
+//2-Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 document.getElementById("elementoOndeVoceEsta").parentNode.style.color = "blue"; 
-console.log(document.getElementById("elementoOndeVoceEsta").firstElementChild.innerText = "O primeiro filho é Icaro!"); 
+
+//3-Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+console.log(document.getElementById("elementoOndeVoceEsta").firstElementChild.innerText = "O primeiro filho é Icaro!");
+
+//4-Acesse o primeiroFilho a partir de pai .
+console.log(document.querySelector('#pai').firstElementChild); 
+
+//5-Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+console.log(document.querySelector('#elementoOndeVoceEsta').parentElement);
+
+//6-Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+console.log(document.querySelector('#elementoOndeVoceEsta').nextSibling);
+
+//7-Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+console.log(document.querySelector('#elementoOndeVoceEsta').childNodes);
