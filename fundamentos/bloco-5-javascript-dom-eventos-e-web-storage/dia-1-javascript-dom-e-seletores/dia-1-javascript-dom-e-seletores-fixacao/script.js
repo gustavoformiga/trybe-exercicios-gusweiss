794df12234483d6c39c953a123c8678c4354a9aa
Gustavo Weiss <gustavoweissformiga@gmail.com>
2021-11-19 12:41:46 -0300

    fixed: added comments to fixation exercises

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/dia-1-javascript-dom-e-seletores-fixacao/script.js
@@ -0,0 +1,8 @@
+// bloco-5-5.1-fixationExercises
+// 1-Adicione uma classe igual para os dois parágrafos.
+// 2-Recupere os seus parágrafos via código JavaScript , usando a função getElementsByClassName ;
+// 3- Altere algum estilo do primeiro deles.
+document.getElementsByClassName("paragraph_test")[0].style.color = "blue";
+
+// 4-Recupere o subtítulo e altere a cor dele usando a função getElementsByTagName .
+document.getElementsByTagName("h4")[0].style.color = "green";
