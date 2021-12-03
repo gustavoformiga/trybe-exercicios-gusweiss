ba263653a85d7082e91e20c027f07a14e06b7f68
seu-usersaneme <gustavoweissformiga@gmail.com>
2021-12-03 18:28:49 -0300

    exercise 1- part1- fixing the scope

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio1.js
@@ -0,0 +1,14 @@
+// 1- Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+// Modifique a estrutura da função para que ela seja uma arrow function .
+
+      function testingScope(escopo) {
+        if (escopo === true) {
+          let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+          ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+          console.log(ifScope);
+        } else {
+          let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+          console.log(elseScope);
+        }
+      }
+      testingScope(false);
