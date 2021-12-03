2f42f67e30ed6e0a29f2ee0c539bc79313c69155
seu-usersaneme <gustavoweissformiga@gmail.com>
2021-12-03 18:34:20 -0300

    exercise 1- part2- turning into arrow function

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio1.js
@@ -1,7 +1,18 @@
-// 1- Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
-// Modifique a estrutura da função para que ela seja uma arrow function .
+// 1-Modifique a estrutura da função para que ela seja uma arrow function .
 
-      function testingScope(escopo) {
+/*       function testingScope(escopo) {
+        if (escopo === true) {
+          let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+          ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+          console.log(ifScope);
+        } else {
+          let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+          console.log(elseScope);
+        }
+      }
+      testingScope(false); */
+
+      const testingScope = (escopo) => {
         if (escopo === true) {
           let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
           ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
