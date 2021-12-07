4d23088fa2d63b552b7b99da9f42d75ee5e58f8e
seu-usersaneme <gustavoweissformiga@gmail.com>
2021-12-07 09:18:41 -0300

    exercise2- part2- creating a function that returns the longest word

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio1.js
@@ -1,8 +1,11 @@
-// 1-Crie uma função que receba um número e retorne seu fatorial.
-const fatorial = (n) => {
-  let result = n;
-  for (let index = 1; index < n; index += 1) {
-    result = result * index;
-  } return result;
+// 2-Crie uma função que receba uma frase e retorne qual a maior palavra.
+const longestWord = (phrase) => {
+  let newLongestWord = phrase.split(' ');
+  let maxSize = 0;
+  for (let word of newLongestWord) {
+    if (word.length > maxSize) {
+    maxSize = word.length;
+    result = word;
+    }
+  } return result
 }
-console.log(fatorial(5));
