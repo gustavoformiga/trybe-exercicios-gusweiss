934d3deebf6004a9e3400e30f758be2bdc6a80a6
seu-usersaneme <gustavoweissformiga@gmail.com>
2021-12-07 08:12:44 -0300

    exercise1 - part4- creating a function that orders the array(used compare function)

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio1.js
@@ -1,12 +1,10 @@
-// 1-part3-Modifique as concatenações para template literals 
-      const testingScope = (escopo) => {
-        if (escopo === true) {
-          let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-          ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-          console.log(ifScope);
-        } else {
-          let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-          console.log(elseScope);
-        }
-      }
-      testingScope(false);
+// 2- Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+function orderingArray() {
+  let newArray = oddsAndEvens.sort(function(a, b){return a - b}); 
+  return newArray;
+}
+
+console.log(orderingArray()); 
+// used a compare function in the sort method to compare all values in the array to dispose in crescent order
