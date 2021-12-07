d152a0047836fdc03bf58aa7d332967b67c93e7d
seu-usersaneme <gustavoweissformiga@gmail.com>
2021-12-07 08:44:04 -0300

    exercise2- part1- creating a function to calculate fatorial

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio1.js
@@ -1,10 +1,8 @@
-// 2- Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
-// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-function orderingArray() {
-  let newArray = oddsAndEvens.sort(function(a, b){return a - b}); 
-  return newArray;
+// 1-Crie uma função que receba um número e retorne seu fatorial.
+const fatorial = (n) => {
+  let result = n;
+  for (let index = 1; index < n; index += 1) {
+    result = result * index;
+  } return result;
 }
-
-console.log(orderingArray()); 
-// used a compare function in the sort method to compare all values in the array to dispose in crescent order
+console.log(fatorial(5));
