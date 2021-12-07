ff2eb22f85ef0100ab7f1c7b919f33d73c00df43
seu-usersaneme <gustavoweissformiga@gmail.com>
2021-12-07 08:00:27 -0300

    exercise1 - part3- concatenating string with template literals

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio1.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicio1.js
@@ -1,21 +1,8 @@
-// 1-Modifique a estrutura da função para que ela seja uma arrow function .
-
-/*       function testingScope(escopo) {
-        if (escopo === true) {
-          let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-          ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-          console.log(ifScope);
-        } else {
-          let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-          console.log(elseScope);
-        }
-      }
-      testingScope(false); */
-
+// 1-part3-Modifique as concatenações para template literals 
       const testingScope = (escopo) => {
         if (escopo === true) {
           let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-          ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+          ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
           console.log(ifScope);
         } else {
           let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
