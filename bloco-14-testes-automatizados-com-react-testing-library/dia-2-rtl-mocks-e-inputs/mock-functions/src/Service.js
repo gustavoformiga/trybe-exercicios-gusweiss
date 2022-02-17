bad0a2cc2a6fa2bf7cd608c95318c8ec8fc6b0bc
gustavoweiss <gustavoweissformiga@gmail.com>
2022-02-17 11:21:14 -0300

    feat: exercises 2 and 3

--- bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/mock-functions/src/Service.js
+++ bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/mock-functions/src/Service.js
@@ -6,4 +6,5 @@ export const randomNumberInt = (min, max) => {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+module.exports = { randomNumberInt };
 
--- bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/mock-functions/src/Service.test.js
+++ bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/mock-functions/src/Service.test.js
@@ -1,23 +1,45 @@
-import React from 'react';
-// import { randomNumberInt } from './Service';
-
-function randomNumberInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const service = require('./Service');
 
 describe('testa a funcao randomNumberInt', () => {
   it('teste se a função foi chamada e quantas vezes foi chamada', () => {
-    randomNumberInt = jest.fn();
-    randomNumberInt();
-    randomNumberInt();
-    expect(randomNumberInt).toHaveBeenCalled();
-    expect(randomNumberInt).toHaveBeenCalledTimes(2);
+    service.randomNumberInt = jest.fn();
+    service.randomNumberInt();
+    service.randomNumberInt();
+    expect(service.randomNumberInt).toHaveBeenCalled();
+    expect(service.randomNumberInt).toHaveBeenCalledTimes(2);
   })
 
   it('testa se a função retorna 10 como padrão', () => {
-    randomNumberInt = jest.fn().mockReturnValue(10);
-    expect(randomNumberInt()).toBe(10);
+    service.randomNumberInt = jest.fn().mockReturnValue(10);
+    expect(service.randomNumberInt()).toBe(10);
+  })
+})
+
+// exercise 2
+describe('Testando mock implementation em randomNumberInt', () => {
+  it('Testar se randomNumberInt mockada foi chamada e se a mockimplementation foi aplicada', () => {
+    service.randomNumberInt.mockImplementation((a,b) => a/b);
+    expect(service.randomNumberInt(12, 2)).toBe(6);
+    expect(service.randomNumberInt).toHaveBeenCalled();
+    expect(service.randomNumberInt).toBeCalledTimes(1);
+    expect(service.randomNumberInt).toHaveBeenCalledWith(12,2);
+  })
+})
+
+// exercise 3
+describe('Testando mock implementation em randomNumberInt com três parâmetros', () => {
+  it('Retornar a multiplicação de 3 parâmetros da mock implementation de randomnumberInt', () => {
+    service.randomNumberInt.mockImplementationOnce((a,b,c) => a*b*c);
+    expect(service.randomNumberInt(2,3,5)).toBe(30);
+    expect(service.randomNumberInt).toHaveBeenCalled();
+    expect(service.randomNumberInt).toBeCalledTimes(1);
+    expect(service.randomNumberInt).toHaveBeenCalledWith(2,3,5);
+    // reset da mock
+    service.randomNumberInt.mockReset();
+    service.randomNumberInt.mockImplementationOnce((a) => a*2);
+    expect(service.randomNumberInt(5)).toBe(10);
+    expect(service.randomNumberInt).toHaveBeenCalled();
+    expect(service.randomNumberInt).toBeCalledTimes(1);
+    expect(service.randomNumberInt).toHaveBeenCalledWith(5);
   })
 })
