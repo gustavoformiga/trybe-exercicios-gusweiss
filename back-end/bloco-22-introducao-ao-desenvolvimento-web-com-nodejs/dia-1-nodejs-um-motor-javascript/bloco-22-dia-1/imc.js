2a5abac31a3fa1ccc34e00d00f11889d26305f33
gustavoweiss <gustavoweissformiga@gmail.com>
2022-05-10 13:54:15 -0300

    exercises 3,4,5

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/bloco-22-dia-1/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/bloco-22-dia-1/imc.js
@@ -1,11 +1,35 @@
+// exercises1,2,3
 const readline = require('readline-sync');
 const peso = readline.questionFloat('Digite seu peso:');
 const altura = readline.questionFloat('Digite sua altura em cm:');
+const alturaMetros = altura/100;
+const imc = peso/(alturaMetros**2);
 
-function imc(peso, altura) {
-  const alturaMetros = altura/100;
-  console.log(`Perfil: ${peso}kg e ${alturaMetros}m`);
-  return `Seu imc é ${peso / alturaMetros ** 2}`;
+function funcImc(peso, altura) {
+  console.log(`Perfil: ${peso}kg e ${alturaMetros/100}m`);
+  return `Seu imc é ${imc}`;
 }
-console.log(imc(peso, altura));
-module.export = imc;
+
+function situacao(funcImc) {
+  if(imc < 18.5) {
+    console.log('Abaixo do peso');
+  }
+  if(imc >= 18,5 && imc <= 24.9) {
+    console.log('Peso normal');
+  }
+  if(imc >= 25 && imc <= 29.9) {
+    console.log('Peso Acima do peso(sobrepeso)');
+  }
+  if(imc >= 30 && imc <= 34.9) {
+    console.log('Peso Obesidade grau I');
+  }
+  if(imc >= 35 && imc <= 39.9) {
+    console.log('Peso Obesidade grau II');
+  }
+  if(imc >= 40) {
+    console.log('Peso Obesidade graus III e IV');
+  }
+};
+
+console.log(funcImc(peso, altura), situacao(funcImc));
+// module.export = funcImc;
