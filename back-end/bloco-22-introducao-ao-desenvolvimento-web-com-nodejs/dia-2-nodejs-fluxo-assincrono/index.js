67c4ced781f78ecd3ccaf1344e04d1cd52ccec83
gustavoweiss <gustavoweissformiga@gmail.com>
2022-05-12 10:40:41 -0300

    exercises 2-then and catch

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/index.js
@@ -13,5 +13,17 @@ function ex(a, b, c) {
   return promise;
 };
 
-console.log(ex(10,10,10));
-console.log(ex(1,1,1));
+// console.log(ex(10,10,10));
+// console.log(ex(1,1,1));
+
+// Escreva um código para consumir a função construída no exercício anterior.
+// Gere um número aleatório de 1 a 100 para cada parâmetro que a função recebe. Para gerar um número aleatório, utilize o seguinte trecho de código: Math.floor(Math.random() * 100 + 1).
+// Chame a função do exercício anterior, passando os três números aleatórios como parâmetros.
+// Utilize then e catch para manipular a Promise retornada pela função:
+function getRandom() {
+  return Math.floor(Math.random() * 100 + 1);
+}
+const randomNumbersArray = [getRandom(), getRandom(), getRandom()]
+ex(...randomNumbersArray)
+  .then(result => console.log(result))
+  .catch(error => console.log(error.message));
