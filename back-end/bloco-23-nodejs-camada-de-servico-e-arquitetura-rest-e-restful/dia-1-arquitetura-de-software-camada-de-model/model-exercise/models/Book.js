a690aed4971260f9a5e2c7e327a96a1481b708bb
gustavoweiss <gustavoweissformiga@gmail.com>
2022-05-20 16:54:46 -0300

    feat: added the function serializeWithCamel to change author_id from db to authorId

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-exercise/models/Book.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-exercise/models/Book.js
@@ -1,8 +1,14 @@
 const connection = require('../db/connection');
 
+const serializeWithCamel = (booksData) => ({
+  id: booksData.id,
+  title: booksData.title,
+  authorId: booksData.author_id,
+});
+
 const getAll = async () => {
   const [row] = await connection.execute('SELECT id, title, author_id FROM model_example.books');
-  return console.log(row), row;
+  return row.map(serializeWithCamel);
 };
 
 module.exports = {
