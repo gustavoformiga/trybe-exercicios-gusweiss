1db340a627b901bf37fc657788826f57793d3e15
gustavoweiss <gustavoweissformiga@gmail.com>
2022-05-20 16:48:37 -0300

    feat and fix: created the route books at index.js and fixed the host at env file

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-exercise/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-exercise/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT;
+const Books = require('./models/Book');
 
 app.use(express.json());
+
+app.get('/books', async (_req, res) => {
+  const books = await Books.getAll();
+  res.status(200).json(books);
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
