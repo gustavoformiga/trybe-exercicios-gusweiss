6a2346493f6eae91c069e6dade3f39703a489dda
gustavoweiss <gustavoweissformiga@gmail.com>
2022-05-20 17:40:01 -0300

    feat: previous exercise1 complete

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-exercise/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-exercise/index.js
@@ -6,10 +6,22 @@ const Books = require('./models/Book');
 
 app.use(express.json());
 
+// getting res by author_id
+app.get('/books', async (req, res) => {
+  try {
+    const { author_id } = req.query;
+    const booksById = (author_id) ? await Books.getByAuthorId(author_id) : await Books.getAll();
+    return res.status(200).json(booksById);
+  } catch (error) {
+    return res.status(500).json({message: error.message});
+  }
+})
+
+// getting all books
 app.get('/books', async (_req, res) => {
   const books = await Books.getAll();
   res.status(200).json(books);
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-exercise/models/Book.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-exercise/models/Book.js
@@ -11,6 +11,16 @@ const getAll = async () => {
   return row.map(serializeWithCamel);
 };
 
+const getByAuthorId = async (authorID) => {
+  const query = 'SELECT id, title, author_id FROM model_example.books WHERE author_id = ?';
+  const [booksData] = await connection.execute(query, [authorID]);
+  if (booksData.length === 0) {
+    return null;
+  }
+  return booksData.map(serializeWithCamel);
+}
+
 module.exports = {
   getAll,
+  getByAuthorId,
 }
