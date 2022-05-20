d2e1f6c1d6cf89fc28c61d1a7b928dd38f7bf399
gustavoweiss <gustavoweissformiga@gmail.com>
2022-05-20 16:23:10 -0300

    feat: previous-exercise- created index.js, models with Author.js, db dir with connection and put env and node_modules at gitignore

---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-exercise/db/connection.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 3306,
+})
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-exercise/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-exercise/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const PORT = process.env.PORT;
 
 app.use(express.json());
-
-const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
